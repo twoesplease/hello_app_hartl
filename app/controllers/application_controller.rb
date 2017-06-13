607bbfb39b259477ee432a65cae5b3e7830e92ec
Tonee Young <tonee.n.you@gmail.com>
2017-06-13 10:18:26 -0400

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+# coding: utf-8
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡hola, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye, cruel world!"
+  end
+
+end
